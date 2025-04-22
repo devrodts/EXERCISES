69667d900f6aef9df3ad09c343f119db480440db
rod <dev_rodts@outlook.com>
2025-04-22 09:11:54 -0300

    feat: add sum java function

---
+++ sum/Sum.java
@@ -0,0 +1,27 @@
+package sum;
+import java.util.Scanner;
+
+public class Sum{
+
+
+    public int SumNumbers(){  
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+        scanner.close();
+        return sum;
+    }
+
+    public static void main(String[] args){
+        Sum sum = new Sum();
+        sum.SumNumbers();
+        System.out.println("Sum method executed successfully.");
+    }
+}
